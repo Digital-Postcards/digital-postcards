5fcc344865d5807d4ca4bbeea399c018b58c5f13
113789566 <89963911+113789566@users.noreply.github.com>
2022-02-14 03:49:41 -0500

    Got carousel....
    There's still the weird cut off with the problem being with either PostcardCarousel or how I'm interacting with Carousel

--- postcards/src/pages/home.js
+++ postcards/src/pages/home.js
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
+import "./home.css"
+import banner from "../resources/placeholder.jpg";
+import image1 from "../resources/kirby_poster.png";
+import image2 from "../resources/snail.jpg";
+import image3 from "../resources/yellow_blocks.png";
+import image4 from "../resources/kirby small.png"
+import PostcardCarousel from "../components/carouselWrapper";
+
+
 function Home(){
-    return (<h1>I'M THE HOME PAGE</h1>)
+    var imageList = [image1, image2, image3, image4];
+    return (
+        <Fragment>
+            {/**************************** Banner **************************************/}
+            <img src={banner} alt="Banner didn't load! Please try again"></img>
+            {/**************************** Carousel **************************************/}
+            <PostcardCarousel imageList={imageList}/>
+        </Fragment>
+
+    )
 }
 export default Home;

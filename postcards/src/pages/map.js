b5a9413ff3159dfc24e9a912ff342297f224f467
May Me Me Maung <maymememaung27@gmail.com>
2022-03-09 12:30:39 -0500

    map-page-update

--- postcards/src/pages/map.js
+++ postcards/src/pages/map.js
@@ -1,5 +1,50 @@
-import React from "react";
-function Map(){
-    return (<h1>I'M THE MAP PAGE</h1>)
+import { map } from "leaflet";
+import React, { useState } from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import Cluster from "../components/Cluster";
+import "../styles/Map.css";
+import mapselectors from "../resources/mapselectors.json";
+
+function Map() {
+  const center = [54.199793, -2.638488];
+  const [map, setMap] = useState(null);
+
+  return (
+    <div>
+      <div id="map">
+        <MapContainer
+          center={center}
+          zoom={3}
+          zoomSnap={0}
+          scrollWheelZoom={true}
+          maxZoom={6}
+          minZoom={0}
+          whenCreated={(map) => setMap(map)}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Cluster />
+        </MapContainer>
+        <div id="map-selector-container">
+        {Object.keys(mapselectors).map((id) => {
+          let loc = mapselectors[id];
+          //console.log(loc.Name);
+          return (
+            <p
+              key={loc.Name}
+              className="map-selector"
+              onClick={() => map.flyTo([loc.Latitude, loc.Longitude])}
+            >
+              {loc.Name}
+            </p>
+          );
+        })}
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default Map;

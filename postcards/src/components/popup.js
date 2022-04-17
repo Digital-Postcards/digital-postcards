e57b1b523276970598cf438322d6af2563f5c655
113789566 <89963911+113789566@users.noreply.github.com>
2022-04-16 23:55:34 -0400

    Change Popup text

--- postcards/src/components/popup.js
+++ postcards/src/components/popup.js
@@ -2,11 +2,11 @@ import "../styles/popup.css"
 function PopUp(props){
     return (<div id="popUp" className="background-blur">
         <div className="popup-box">
-            <span className="close-icon" onClick={()=>{
-                document.getElementById("popUp").style.display = "none"}
-            }>x</span>
-            <h1 id="triggerTitle">TRIGGER WARNING</h1>
-            <p>The following content may be sensitive. Please proceed with caution.</p>
+            <h1 id="triggerTitle">CONTENT WARNING</h1>
+            <p>This digital history exhibition contains degrading depictions of women and people of color from the late 1800s and early 1900s</p>
+            <button className="confirmButton" onClick={()=>{document.getElementById("popUp").style.display = "none"}}>
+                Please click if you wish to continue
+            </button>
         </div>
     </div>)
 }
